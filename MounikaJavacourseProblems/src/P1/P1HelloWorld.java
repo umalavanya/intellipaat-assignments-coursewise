d673c24e576817eed358838bab931b8589c3f6ce
umalavanya <umalavanya7@gmail.com>
2026-07-09 14:21:06 -0400

    Java Practice

---
+++ MounikaJavacourseProblems/src/P1/P1HelloWorld.java
@@ -0,0 +1,9 @@
+package P1;
+
+public class P1HelloWorld {
+	public static void main(String[] args){
+		System.out.println("Hello World") ;
+
+	}
+
+}
---
+++ MounikaJavacourseProblems/src/P2/P2PrintName.java
@@ -0,0 +1,9 @@
+package P2;
+
+public class P2PrintName {
+	
+	public static void main(String[] args) {
+		System.out.println("My name is Uma")  ;
+	}
+ 
+}
