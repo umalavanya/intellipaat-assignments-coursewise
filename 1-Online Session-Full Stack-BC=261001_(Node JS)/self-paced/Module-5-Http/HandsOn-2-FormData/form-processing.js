82f4628f3c52b1949bce9ef76cd0d3fb70192c45
umalavanya <umalavanya7@gmail.com>
2026-07-12 18:39:02 -0400

    http-request
    There is an error in this code.

---
+++ 1-Online Session-Full Stack-BC=261001_(Node JS)/self-paced/Module-5-Http/HandsOn-2-FormData/form-processing.js
@@ -0,0 +1,20 @@
+const http = require('http');
+const fs = require('fs');
+
+http.createServer((req,res) => {
+    if(req.method == "POST"){
+        let body = '';
+        req.on('data', chunk =>{
+            body += chunk.toString();
+        });
+        req.on('end', () =>{
+            console.log(body);
+            res.end('ok');
+        });
+    } else {
+        fs.readFile('./form-processing.html',(err,data) =>{
+            res.writeHead(200, {'Content-Type': 'text/html'});
+            res.end(data);
+        });
+    }
+}).listen(8080);
