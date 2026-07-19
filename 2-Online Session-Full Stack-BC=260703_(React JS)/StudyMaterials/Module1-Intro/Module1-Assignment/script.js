dd665413e9b46f6171f6a7afb0a3442502a87ea4
umalavanya <umalavanya7@gmail.com>
2026-07-19 01:19:41 -0400

    practice React

---
+++ 2-Online Session-Full Stack-BC=260703_(React JS)/StudyMaterials/Module1-Intro/Module1-Assignment/script.js
@@ -0,0 +1,9 @@
+const input = document.getElementById('input') ;
+const add_task = document.getElementById('add-task') ;
+
+
+function handleOnClick(e){
+
+    add_task.appendChild(<li>e.target.value</li>)
+    
+}
