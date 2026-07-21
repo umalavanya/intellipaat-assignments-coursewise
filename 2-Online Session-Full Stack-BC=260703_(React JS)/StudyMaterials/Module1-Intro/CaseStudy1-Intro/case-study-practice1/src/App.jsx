a2635e632a08dbf02cdd9259aa3a6b125ab26a72
umalavanya <umalavanya7@gmail.com>
2026-07-21 00:31:59 -0400

    Case study in react practice

---
+++ 2-Online Session-Full Stack-BC=260703_(React JS)/StudyMaterials/Module1-Intro/CaseStudy1-Intro/case-study-practice1/src/App.jsx
@@ -0,0 +1,45 @@
+import React, { useState} from 'react' ;
+import './App.css' ;
+
+const employees = [
+  {"id":1 , "name": "Alice Johnson" ,"department":"Tech" , "position":"Developer"  } ,
+  {"id":2, "name":"Bob smith" ,"department":"HR" , "position":  "HR Manager"},
+  {"id":3 , "name": "Charlie Brown","department": "Finance", "position": "Accountant" },
+  {"id":4 , "name": "Diana Prince" ,"department": "Tech", "position":  "Frontend Developer"}
+
+]
+
+
+function App(){
+
+
+  const departments = [ "all", ...new Set(employees.map(emp => emp.department))] ;
+  return(
+  
+    <div className="app-container">
+
+      <header classname="app-header">
+        <h1>Employee Directory</h1>
+        <p classname="subtitle">View and filter employees</p>
+      </header>
+
+      {/* Filter bar */}
+      <div className="filter-bar">
+        <span className="filter-label">Filter by department: </span>
+        <div className="filter-group">
+          {
+
+            departments.map(dept => (
+            ))
+          }
+
+
+
+        </div>
+
+      </div>
+    </div>
+  ) ;
+} 
+
+export default App ;
