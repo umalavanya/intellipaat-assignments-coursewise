86356a734097eb259ce3844eb9a0feb40e8331e7
umalavanya <umalavanya7@gmail.com>
2026-07-24 23:28:45 -0400

    Create Dawn.java

---
+++ MounikaJavacourseProblems/src/P9/Dawn.java
@@ -0,0 +1,22 @@
+package P9;
+
+public class Dawn {
+
+	public static void main(String[] args) {
+		
+		
+		System.out.println("Hello!!");
+		
+		for(int i=1; i<=5 ; i++) {
+			for(int j = 1 ; j <= 5 ; j++) {
+				System.out.print(j);
+			}
+			
+			System.out.println("");
+			
+		}
+		
+
+	}
+
+}
