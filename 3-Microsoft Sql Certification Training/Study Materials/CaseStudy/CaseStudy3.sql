85ba8fe8920cf929aa3cc3d46cd06ccda80e13a1
umalavanya <umalavanya7@gmail.com>
2026-08-16 05:42:32 -0400

    Update CaseStudy3.sql

--- 3-Microsoft Sql Certification Training/Study Materials/CaseStudy/CaseStudy3.sql
+++ 3-Microsoft Sql Certification Training/Study Materials/CaseStudy/CaseStudy3.sql
@@ -1,5 +1,5 @@
 --Problem Statement:
---You are the database developer of an international bank. You are responsible formanaging the bank’s database. 
+--You are the database developer of an international bank. You are responsible for managing the bank’s database. 
 --You want to use the data to answer a few questions about your customers regarding withdrawal, deposit and so on, especially about the transaction amount on a particular date across various regions of the world. 
 --Perform SQL queries to get the key insights of a customer.
 
