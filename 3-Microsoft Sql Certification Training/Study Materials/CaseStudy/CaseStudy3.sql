363f09a93fea57a44930e3e37d34ef79e6830865
umalavanya <umalavanya7@gmail.com>
2026-08-18 14:31:50 -0400

    SQL Practice

--- 3-Microsoft Sql Certification Training/Study Materials/CaseStudy/CaseStudy3.sql
+++ 3-Microsoft Sql Certification Training/Study Materials/CaseStudy/CaseStudy3.sql
@@ -4,6 +4,7 @@
 --Perform SQL queries to get the key insights of a customer.
 
 --Dataset:
+
 --The 3 key datasets for this case study are:
 --a. Continent: The Continent table has two attributes i.e., region_id and region_name, where region_name consists of different continents such as Asia, Europe, Africa etc., assigned with the unique region id.
 --b. Customers: The Customers table has four attributes named customer_id, region_id, start_date and end_date which consists of 3500 records.
--- 3-Microsoft Sql Certification Training/Study Materials/TryYourself/TryYourSelf.sql
+++ 3-Microsoft Sql Certification Training/Study Materials/TryYourself/TryYourSelf.sql
@@ -1,31 +1,22 @@
-Create the following Tables and insert the values accordingly and
-perform the tasks mentioned below:
+--Create the following Tables and insert the values accordingly and perform the tasks mentioned below:
 Employee Table
-Employee Bonus
-Table
+Employee Bonus Table
 
 Employee Title Table
+
 Tasks To Be Performed:
-1. Display the FIRST_NAME from the Employee Table using the alias name
-as Employee_name.
+1. Display the FIRST_NAME from the Employee Table using the alias name as Employee_name.
 2. Display LAST_NAME from the Employee Table in upper case.
 3. Display unique values of DEPARTMENT from the Employee Table.
-4. Display the first three characters of LAST_NAME from the Employee
-Table.
-5. Display the unique values of DEPARTMENT from the Employee Table and
-print its length.
-6. Display the FIRST_NAME and LAST_NAME from the Employee Table into
-a single column as FULL_NAME. A space char should separate them.
-7. Display all employee details from the Employee Table order by
-FIRST_NAME ascending.
-8. Display all employee details ordered by FIRST_NAME ascending and
-DEPARTMENT descending.
+4. Display the first three characters of LAST_NAME from the Employee Table.
+5. Display the unique values of DEPARTMENT from the Employee Table and print its length.
+6. Display the FIRST_NAME and LAST_NAME from the Employee Table into  single column as FULL_NAME. A space char should separate them.
+7. Display all employee details from the Employee Table order by   FIRST_NAME ascending.
+8. Display all employee details ordered by FIRST_NAME ascending and DEPARTMENT descending.
 
-9. Display details of employees with the first name as “VEENA ' and
-“KARAN” from the Employee Table.
-10. Display details of employees with DEPARTMENT name as “Admin”.
-11.
-Display details of the employees whose FIRST_NAME contains ‘V’.
+9. Display details of employees with the first name as VEENA and KARAN from the Employee Table.
+10. Display details of employees with DEPARTMENT name as Admin.
+11.Display details of the employees whose FIRST_NAME contains ‘V’.
 12. Display details of the employees whose salary lies between 100000 and
 500000.
 13. Display details of the employees who have joined in February, 2020.
@@ -43,8 +34,7 @@ table.
 23. Display the first 50% records from a table.
 24. Display the departments that have less than 4 people in it.
 25. Display all departments along with the number of people in there.
-26. Display the name of employees having the highest salary in each
-department.
+26. Display the name of employees having the highest salary in each department.
 27. Display the names of employees who earn the highest salary.
 28. Display the average salaries for each department.
 29. Display the name of the employee who got the maximum bonus.
