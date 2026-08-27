18a5a67ab6ab115c7ed9e5cbaaf97aa504e8add0
umalavanya <umalavanya7@gmail.com>
2026-08-27 10:07:16 -0400

    task manager app

--- 100-PRT Preparation/Problem5-TaskManagerApp-Practice/backend/server.js
+++ 100-PRT Preparation/Problem5-TaskManagerApp-Practice/backend/server.js
@@ -1,15 +1,12 @@
 const express = require('express') ;
 const cors = require('cors') ;
 
-
 const app = express() ;
 
 app.use(express.json());
-
 app.use(cors()) ;
 
-app.get('/api/test/', (res) =>{
-    
+app.get('/api/test/', (req, res) =>{
     res.status(200).json('Working') ;
 })
 
