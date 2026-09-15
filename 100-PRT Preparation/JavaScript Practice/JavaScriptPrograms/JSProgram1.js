9d098af7d0d544779d486263a908d8d83ce7e5f6
umalavanya <umalavanya7@gmail.com>
2026-09-15 09:23:56 -0400

    practice

---
+++ 100-PRT Preparation/JavaScript Practice/JavaScriptPrograms/JSProgram1.js
@@ -0,0 +1,16 @@
+
+
+console.log(typeof null);
+console.log(typeof String);
+console.log(typeof Number)
+console.log(typeof Boolean);
+console.log(typeof bigint);
+console.log(typeof Symbol);
+console.log(typeof undefined);
+
+
+
+;
+
+
+
